bb8428d8382b7839942b4003fc1ad0805debff8d
Suhag Al Amin <developersuhag@gmail.com>
2021-10-14 11:21:59 +0600

    tour card

--- js/app.js
+++ js/app.js
@@ -16,3 +16,59 @@ function closeMenu() {
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
 }
+
+const loadData = () => {
+  fetch("../tours.json")
+    .then((res) => res.json())
+    .then((data) => showTourOnUi(data));
+};
+loadData();
+
+// tour packages
+
+const showTourOnUi = (tours) => {
+  const tourContainer = document.getElementById("all-packages-container");
+
+  tours.forEach((tour) => {
+    const { tour_title, price, img, country, description, rating, badge } =
+      tour;
+    const card = document.createElement("div");
+    card.classList.add("tour-card");
+    card.innerHTML = `
+        <div class="p-4 h-full">
+            <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                  <img class="lg:h-60 md:h-36 w-full object-cover object-center" src=${img} alt=${tour_title}>
+                <div class="p-6">
+                      <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
+                      <h1 class="title-font text-lg  font-bold text-gray-900 mb-3">${tour_title}</h1>
+                      <p class="leading-relaxed mb-3">${description.slice(
+                        0,
+                        100
+                      )}</p>
+                    <div class="flex items-center flex-wrap ">
+                          <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                            <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                              <path d="M5 12h14"></path>
+                              <path d="M12 5l7 7-7 7"></path>
+                            </svg>
+                          </a>
+                          <span class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 b     order-r-2 border-gray-200">
+                            <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                              <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                              <circle cx="12" cy="12" r="3"></circle>
+                            </svg>1.2K
+                          </span>
+                          <span class="text-gray-400 inline-flex items-center leading-none text-sm">
+                            <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                              <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                            </svg>6
+                          </span>
+                    </div>
+                </div>    
+            </div>
+        </div>
+
+    `;
+    tourContainer.appendChild(card);
+  });
+};
